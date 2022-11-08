4466f9e0aab596d627b42880575a16d123f8f4d5
Tom Gregan <103416795+SEO-Tom@users.noreply.github.com>
2022-11-08 16:31:08 +0000

    Fix selector

--- add-numbers-to-SERP.js
+++ add-numbers-to-SERP.js
@@ -1,4 +1,4 @@
-let nodes = document.querySelectorAll("#rso > div.g,#rso > div > div.g");
+let nodes = document.querySelectorAll("h3.LC20lb");
 let nodesAsArray = Array.from(nodes);
 nodesAsArray.forEach(node => {
     node.insertAdjacentHTML("beforeBegin",`<p class="serp-numbers">${nodesAsArray.indexOf(node) + 1}</p>`);
